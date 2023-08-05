63917413fff1243208efeb440ce9b47a734f8deb
daviszung <daviszung@gmail.com>
2023-08-04 23:11:12 -0400

    implemented agent details button

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import { Navbar } from "./Navbar";
+
+import { AgentDetails } from "./Navbar";
 
 
 export function App() {
+    const [agentDetails, setAgentDetails] = useState<AgentDetails>();
+
+    console.log(agentDetails);
 
     return (
         <div className="h-screen bg-slate-900 text-white">
-            <Navbar />
-            <main></main>
-            
+            <Navbar setAgentDetails={setAgentDetails} />
+            <main>
+                {agentDetails &&
+                    <ul>
+                        <li>{agentDetails.symbol}</li>
+                        <li>{agentDetails.headquarters}</li>
+                        <li>{agentDetails.credits}</li>
+                        <li>{agentDetails.startingFaction}</li>
+                    </ul>
+                }
+            </main>
+
         </div>
     );
 };
--- src/client/Navbar.tsx
+++ src/client/Navbar.tsx
@@ -1,5 +1,16 @@
+export type AgentDetails = {
+    accountId: string;
+    symbol: string;
+    headquarters: string;
+    credits: number;
+    startingFaction: string;
+};
 
-export function Navbar() {
+type NavbarProps = {
+    setAgentDetails: Function
+}
+
+export function Navbar({setAgentDetails}: NavbarProps) {
 
     async function requestAgentDetails() {
         const res = await fetch('/agent', {
@@ -7,14 +18,20 @@ export function Navbar() {
                 "content-type": "application/json",
             }
         });
-        const body = await res.json();
+        const body: AgentDetails = await res.json();
         console.log(body);
+        return body;
     }
 
     return (
         <nav className="flex items-center px-16 border-b border-emerald-700">
             <div className="px-32 py-10">
-                <button onClick={requestAgentDetails} className="rounded-xl bg-emerald-500 px-4 py-2 text-white font-semibold">Agent</button>
+                <button onClick={async () => {
+                    const agentDetails = await requestAgentDetails();
+                    setAgentDetails(agentDetails)
+
+                }} className="rounded-xl bg-emerald-500 px-4 py-2 text-white font-semibold">Agent</button>
+
             </div>
         </nav>
     );
